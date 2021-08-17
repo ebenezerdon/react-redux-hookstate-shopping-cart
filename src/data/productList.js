aa368f87093832ddccbc5b757ba6b1652a44df69
ebenezerdon <ebenezerdonu@gmail.com>
2021-08-17 15:56:15 +0100

    use JavaScript file for product list

---
+++ src/data/productList.js
@@ -0,0 +1,59 @@
+const productList = [
+  {
+    id: '1',
+    name: 'First Awesome Product',
+    detail: 'First product detail here.',
+    price: {
+      value: 70,
+      currency: 'USD',
+      symbol: '$'
+    },
+    imageUrl: require('./images/products/bubbles-gumball-apron-black.jpg')
+  },
+  {
+    id: '2',
+    name: 'Second Awesome Product',
+    detail: 'Second product detail here.',
+    price: {
+      value: 129,
+      currency: 'USD',
+      symbol: '$'
+    },
+    imageUrl: require('./images/products/bubbles-gumball-apron-gray.jpg')
+  },
+  {
+    id: '3',
+    name: 'Third Awesome Product',
+    detail: 'First product detail here.',
+    price: {
+      value: 91,
+      currency: 'USD',
+      symbol: '$'
+    },
+    imageUrl: require('./images/products/bubbles-gumball-apron-white.jpg')
+  },
+  {
+    id: '4',
+    name: 'Fourth Awesome Product',
+    detail: 'Fourth product detail here.',
+    price: {
+      value: 47,
+      currency: 'USD',
+      symbol: '$'
+    },
+    imageUrl: require('./images/products/bubbles-gumball-baseballhat-black.jpg')
+  },
+  {
+    id: '5',
+    name: 'Fifth Awesome Product',
+    detail: 'Fifth product detail here.',
+    price: {
+      value: 95,
+      currency: 'USD',
+      symbol: '$'
+    },
+    imageUrl: require('./images/products/bubbles-gumball-mug-black.jpg')
+  }
+]
+
+export default productList
