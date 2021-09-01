baf401ac138e39bca158654975c9a8b3e312d796
Marvel <67187047+marveldev@users.noreply.github.com>
2021-09-01 15:30:12 +0100

    create cart page (#9)
    * add styling for cart page
    
    * remove unused css styling from quantity buttons div container in product catalogue  component.
    
    * implement cart page design for mobile screens.
    
    * implement cart page design for mobile screens.
    
    * adjust cart page design for mobile screens.
    
    * add margin top to remove-button
    
    Co-authored-by: ebenezerdon <ebenezerdonu@gmail.com>

--- src/components/cart/Cart.js
+++ src/components/cart/Cart.js
@@ -1,7 +1,41 @@
+import './cart.scss'
+import photoUrlTest from '../../data/images/products/bubbles-gumball-apron-black.jpg'
+
 const Cart = () => {
   return (
     <div className="cart">
-      <h1>Cart</h1>
+      <h3 className="header">Cart (2 items)</h3>
+      <div className="row">
+        <div className="item-image col-lg-3 col-sm-2">
+          <img src={photoUrlTest} alt="product-image" />
+        </div>
+          <div className="item-info col-lg-7 col-sm-5">
+            <h4>Mens Casual Premium Slim Fit T-Shirts</h4>
+            <p className="text-truncate">Slim-fitting style, light weight & soft fabric for
+              breathable and comfortable wearing.
+            </p>
+            <button className="btn btn-primary me-md-2 remove-button">
+              <i className="bi bi-trash-fill"/> Remove Item
+            </button>
+          </div>
+        <div className="item-info col-lg-2 col-sm-3">
+          <div className="qty-buttons">
+            <button className="btn btn-primary qty-left">
+              -
+            </button>
+            <span>1</span>
+            <button className="btn btn-primary qty-right">
+              +
+            </button>
+          </div>
+          <p className="item-price">Price: <span>$687.93</span></p>
+        </div>
+      </div>
+      <footer className="text-center">
+        <p className="total-price">Total Price: <span>$4349.344</span></p>
+        <button className="btn back-button">CONTINUE SHOPPING</button>
+        <button className="btn btn-primary">CHECKOUT</button>
+      </footer>
     </div>
   )
 }
