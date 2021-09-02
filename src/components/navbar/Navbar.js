d24cf6750f116d4910389daad3db357962dcc1ae
Marvel <67187047+marveldev@users.noreply.github.com>
2021-09-02 15:56:45 +0100

    Add cart number feature (#12)
    * add styling for cart page
    
    * remove unused css styling from quantity buttons div container in product catalogue  component.
    
    * add number of items in Cart to nav bar.
    
    * add number of items in Cart to nav bar.

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -4,13 +4,14 @@ import './navbar.scss'
 const Navbar = () => {
   return (
     <nav className="navbar">
-        <i className="bi bi-shop-window"/>
-        <NavLink to="/cart" activeClassName="selected" title="cart" className="link">
-          <i className="bi bi-cart3"/>
-        </NavLink>
-        <NavLink exact to="/" activeClassName="selected" title="products">
-          <i className="bi bi-grid"/>
-        </NavLink>
+      <i className="bi bi-shop-window"/>
+      <NavLink to="/cart" activeClassName="selected" title="cart" className="link">
+        <i className="bi bi-cart3"/>
+        <sup className="cart-number">2</sup>
+      </NavLink>
+      <NavLink exact to="/" activeClassName="selected" title="products">
+        <i className="bi bi-grid"/>
+      </NavLink>
     </nav>
   )
 }
