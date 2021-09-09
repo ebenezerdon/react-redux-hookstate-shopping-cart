03c35fc3c1147d7ba07884363fd5a10db6bd7a51
Marvel <67187047+marveldev@users.noreply.github.com>
2021-09-09 10:18:48 +0100

    create and design empty cart page. (#22)
    - add declaration to display cart product if any, else display empty cart.
    
    [finishes #21]

--- src/components/cart/Cart.js
+++ src/components/cart/Cart.js
@@ -11,38 +11,52 @@ const Cart = () => {
 
   return (
     <div className="cart">
-      <h3 className="header">Cart ({cartProducts.length} items)</h3>
-      {cartProducts?.map(product =>
-        <div key={product.id} className="row">
-          <div className="item-image col-lg-3 col-sm-2">
-            <img src={product.imageUrl.default} alt="product" />
-          </div>
-          <div className="item-info col-lg-7 col-sm-5">
-            <h4>{product.name}</h4>
-            <p className="text-truncate">{product.detail}</p>
-            <button className="btn btn-primary me-md-2 remove-button">
-              <i className="bi bi-trash-fill"/> Remove Item
-            </button>
-          </div>
-          <div className="item-info col-lg-2 col-sm-3">
-            <div className="qty-buttons">
-              <button className="btn btn-primary qty-left">
-                -
-              </button>
-              <span>1</span>
-              <button className="btn btn-primary qty-right">
-                +
-              </button>
+      {cartProducts.length >=1 &&
+        <div className="cart-product">
+          <h3 className="header">Cart ({cartProducts.length} items)</h3>
+          {cartProducts?.map(product =>
+            <div key={product.id} className="row">
+              <div className="item-image col-lg-3 col-sm-2">
+                <img src={product.imageUrl.default} alt="product" />
+              </div>
+              <div className="item-info col-lg-7 col-sm-5">
+                <h4>{product.name}</h4>
+                <p className="text-truncate">{product.detail}</p>
+                <button className="btn btn-primary me-md-2 remove-button">
+                  <i className="bi bi-trash-fill"/> Remove Item
+                </button>
+              </div>
+              <div className="item-info col-lg-2 col-sm-3">
+                <div className="qty-buttons">
+                  <button className="btn btn-primary qty-left">
+                    -
+                  </button>
+                  <span>1</span>
+                  <button className="btn btn-primary qty-right">
+                    +
+                  </button>
+                </div>
+                <p className="item-price">
+                  Price: <span>{product.price.symbol}{product.price.value}</span>
+                </p>
+              </div>
             </div>
-            <p className="item-price">Price: <span>{product.price.symbol}{product.price.value}</span></p>
-          </div>
+          )}
+          <footer className="text-center">
+            <p className="total-price">Total Price: <span>$4349.344</span></p>
+            <button className="btn back-button">CONTINUE SHOPPING</button>
+            <button className="btn btn-primary">CHECKOUT</button>
+          </footer>
         </div>
-      )}
-      <footer className="text-center">
-        <p className="total-price">Total Price: <span>$4349.344</span></p>
-        <button className="btn back-button">CONTINUE SHOPPING</button>
-        <button className="btn btn-primary">CHECKOUT</button>
-      </footer>
+      }
+      {cartProducts.length < 1 &&
+        <div className="text-center empty-cart">
+          <i className="bi bi-cart3"/>
+          <p>Your cart is empty.</p>
+          <p>You have not added any item to your cart.</p>
+          <button className="btn btn-primary">Start Shopping</button>
+        </div>
+      }
     </div>
   )
 }
