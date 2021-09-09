d96ea0149c4e4b6f6067cad47573b0ed0ac94bd2
Marvel <67187047+marveldev@users.noreply.github.com>
2021-09-09 15:22:15 +0100

    add functionality for quantity buttons in cart page. (#24)
    [finishes #23]

--- src/components/cart/Cart.js
+++ src/components/cart/Cart.js
@@ -1,13 +1,30 @@
-import { useSelector } from 'react-redux'
-import productList from '../../data/productList'
+import { useDispatch, useSelector } from 'react-redux'
+import _ from 'lodash'
 import './cart.scss'
+import { cartActions } from './slice'
+import productList from '../../data/productList'
 
 const Cart = () => {
   const cartState = useSelector(state => state.cart)
+  const dispatch = useDispatch()
   const set = new Set(cartState.productIds)
   const cartProductIds = [...set]
 
   const cartProducts = productList.filter(product => cartProductIds.includes(product.id))
+
+  const addToCart = productId => {
+    dispatch(cartActions.addToCart(productId))
+  }
+
+  const removeFromCart = productId => {
+    dispatch(cartActions.removeFromCart(productId))
+  }
+
+  const productPrice = cartProducts.map(
+    product => product.price.value * _.countBy(cartState.productIds)[product.id]
+  )
+
+  const totalPrice = productPrice.reduce((firstValue, secondValue) => firstValue + secondValue, 0)
 
   return (
     <div className="cart">
@@ -28,22 +45,28 @@ const Cart = () => {
               </div>
               <div className="item-info col-lg-2 col-sm-3">
                 <div className="qty-buttons">
-                  <button className="btn btn-primary qty-left">
+                  <button className="btn btn-primary qty-left"
+                          onClick={() => removeFromCart(product.id)}
+                  >
                     -
                   </button>
-                  <span>1</span>
-                  <button className="btn btn-primary qty-right">
+                  <span>{_.countBy(cartState.productIds)[product.id]}</span>
+                  <button className="btn btn-primary qty-right"
+                          onClick={() => addToCart(product.id)}
+                  >
                     +
                   </button>
                 </div>
-                <p className="item-price">
-                  Price: <span>{product.price.symbol}{product.price.value}</span>
+                <p className="item-price">Price:
+                  <span>{product.price.symbol}
+                    {product.price.value * _.countBy(cartState.productIds)[product.id]}
+                  </span>
                 </p>
               </div>
             </div>
           )}
           <footer className="text-center">
-            <p className="total-price">Total Price: <span>$4349.344</span></p>
+            <p className="total-price">Total Price: <span>${totalPrice}</span></p>
             <button className="btn back-button">CONTINUE SHOPPING</button>
             <button className="btn btn-primary">CHECKOUT</button>
           </footer>
