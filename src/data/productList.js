702e27177fe680094efb3182d06cee97a8d9ae5c
Marvel <67187047+marveldev@users.noreply.github.com>
2021-09-16 12:12:30 +0100

    Replace current images in app and add more products with real names (#46)
    * - add new product images
    - remove previous product images
    - add the object fit css property to product image styling.
    
    [finishes #45]
    
    * add products with real names and details
    
    [finishes #45]

--- src/data/productList.js
+++ src/data/productList.js
@@ -1,58 +1,123 @@
 const productList = [
   {
     id: '1',
-    name: 'First Awesome Product',
-    detail: 'First product detail here.',
+    name: 'Fashion Leather Hand Bag',
+    detail: 'It is made of high quality materials, durable enough for your daily wearing',
     price: {
       value: 70,
-      currency: 'USD',
       symbol: '$'
     },
-    imageUrl: require('./images/products/bubbles-gumball-apron-black.jpg')
+    imageUrl: require('./images/products/pexels-diana-akhmetianova-4830926.jpg')
   },
   {
     id: '2',
-    name: 'Second Awesome Product',
-    detail: 'Second product detail here.',
+    name: 'Ladies Trendy Mini Hand Bag',
+    detail: 'Crafted with a premium leather and featuring top design that stand the test of time',
     price: {
       value: 129,
-      currency: 'USD',
       symbol: '$'
     },
-    imageUrl: require('./images/products/bubbles-gumball-apron-gray.jpg')
+    imageUrl: require('./images/products/pexels-dominika-roseclay-5462562.jpg')
   },
   {
     id: '3',
-    name: 'Third Awesome Product',
-    detail: 'First product detail here.',
+    name: 'HOT Mens Formal Leather Shoes',
+    detail: 'Keep your cool and update your footwear collection with this classic, versatile design',
     price: {
       value: 91,
-      currency: 'USD',
       symbol: '$'
     },
-    imageUrl: require('./images/products/bubbles-gumball-apron-white.jpg')
+    imageUrl: require('./images/products/pexels-milan-4637874.jpg')
   },
   {
     id: '4',
-    name: 'Fourth Awesome Product',
-    detail: 'Fourth product detail here.',
+    name: 'Female High-Waist Skinny Jeans Trouser',
+    detail: 'This remarkable styled Jeans is a must have for every lady, it goes for all colors of tops. Its stylish and remarkable.',
     price: {
       value: 47,
-      currency: 'USD',
       symbol: '$'
     },
-    imageUrl: require('./images/products/bubbles-gumball-baseballhat-black.jpg')
+    imageUrl: require('./images/products/pexels-mnz-1598507.jpg')
   },
   {
     id: '5',
-    name: 'Fifth Awesome Product',
-    detail: 'Fifth product detail here.',
+    name: 'Blue Suit For Executive Men',
+    detail: 'Made from high quality fabric to suite all your occasion 100% cotton.',
     price: {
       value: 95,
-      currency: 'USD',
       symbol: '$'
     },
-    imageUrl: require('./images/products/bubbles-gumball-mug-black.jpg')
+    imageUrl: require('./images/products/pexels-nothing-ahead-6375747.jpg')
+  },
+  {
+    id: '6',
+    name: 'Crocodile Pattern Office Women Bag',
+    detail: 'This is a stylish hand bag, We designed this portable bag to make it easier for women to carry personal items when they go out',
+    price: {
+      value: 89,
+      symbol: '$'
+    },
+    imageUrl: require('./images/products/pexels-skylar-kang-6044266.jpg')
+  },
+  {
+    id: '7',
+    name: 'High Quality Female Royal Heel Shoes',
+    detail: 'Its easy to wear and maintain. It is carefully made from genuine quality material that guarantees its durability',
+    price: {
+      value: 45,
+      symbol: '$'
+    },
+    imageUrl: require('./images/products/pexels-terje-sollie-273930.jpg')
+  },
+  {
+    id: '8',
+    name: 'Women Formal Crop Blazer - Grey',
+    detail: 'Produced from best quality cotton fabric, it is a necessity in every lady\'s wardrobe',
+    price: {
+      value: 12,
+      symbol: '$'
+    },
+    imageUrl: require('./images/products/pexels-skylar-kang-6046226.jpg')
+  },
+  {
+    id: '9',
+    name: 'Classy Ladies Trending Bum Short',
+    detail: 'This comfortable short for queens goes for both formal or casual outfit, and party wear',
+    price: {
+      value: 39,
+      symbol: '$'
+    },
+    imageUrl: require('./images/products/pexels-skylar-kang-6046227.jpg')
+  },
+  {
+    id: '10',
+    name: 'Elegant Long Women Palazzo Pant',
+    detail: 'This is a stretchy palazzo trouser made with Lycra material that allows air in and out. It is classy and can be worn with tops and or jackets',
+    price: {
+      value: 102,
+      symbol: '$'
+    },
+    imageUrl: require('./images/products/pexels-skylar-kang-6046228.jpg')
+  },
+  {
+    id: '11',
+    name: 'New Short-sleeved Oversize T-shirt',
+    detail: 'It is very classy and suitable for every occasions. It cool, comfortable and gives an outstanding appearance.',
+    price: {
+      value: 82,
+      symbol: '$'
+    },
+    imageUrl: require('./images/products/pexels-skylar-kang-6046231.jpg')
+  },
+  {
+    id: '12',
+    name: 'Baby Denim Shoes Soft Sole Prewalker',
+    detail: 'Lovely and Charming design, Keeps your baby\'s feet soft and warm, Makes your baby more lovely',
+    price: {
+      value: 55,
+      symbol: '$'
+    },
+    imageUrl: require('./images/products/pexels-votsis-panagiotis-3565860.jpg')
   }
 ]
 
